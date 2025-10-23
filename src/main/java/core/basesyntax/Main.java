f0dab830888cf35b0d3a9827158bb83d9b47d578
isher2k1 <ikekches@gmail.com>
2025-10-23 17:47:24 +0300

    Fixed issues #1

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,14 +3,15 @@ package core.basesyntax;
 public class Main {
     public static void main(String[] args) {
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure figure1 = figureSupplier.getRandomFigure();
-        Figure figure2 = figureSupplier.getRandomFigure();
-        Figure figure3 = figureSupplier.getRandomFigure();
-        Figure figure4 = figureSupplier.getDefaultFigure();
-        Figure figure5 = figureSupplier.getDefaultFigure();
-        Figure figure6 = figureSupplier.getDefaultFigure();
+        Figure[] figures = new Figure[6];
 
-        Figure[] figures = new Figure[]{figure1, figure2, figure3, figure4, figure5, figure6};
+        for (int i = 0; i < 3; i++) {
+            figures[i] = figureSupplier.getRandomFigure();
+        }
+
+        for (int i = 3; i < 6; i++) {
+            figures[i] = figureSupplier.getDefaultFigure();
+        }
 
         for (Figure figure : figures) {
             figure.draw();
